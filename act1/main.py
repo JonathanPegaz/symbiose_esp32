20da18ccdd503e1b8191120d1ea7fddc08657879
JonathanPegaz <pegazjonathan@gmail.com>
2023-01-01 23:47:54 +0100

    add led act 1

--- act1/main.py
+++ act1/main.py
@@ -1,33 +1,67 @@
 from machine import Pin, ADC
 from time import sleep
 import bluetooth
+import neopixel
 from ble_advertising import advertising_payload
 from ble_uart_peripheral import *
 
 ble = bluetooth.BLE()
 uart = BLEUART(ble, "esp1")
 
+isActivated = 0
+
+def on_rx():
+    res = uart.read().decode().strip()
+    print(res)
+    if res == "ok":
+        global isActivated
+        isActivated = 1
+        print(isActivated)
+
+uart.irq(handler=on_rx)
+
+#Anemometer
 # Resistor Divider at ADC input
 R_BRIDGE_RATIO = 0.68117
-
-ana = ADC(Pin(27))
+pinAnemo = Pin(34, Pin.IN)
+ana = ADC(pinAnemo)
+#ana2 = ADC(Pin(14))
 ana.atten( ADC.ATTN_11DB ) # Full 3.3V Range
 
-while True:
-   value = ana.read() # 0..4095
-   v_esp = 3.3 * value / 4096
-   v_anem = v_esp / R_BRIDGE_RATIO
-   # Vitesse vent m/h
-   speed_mps = 6 * v_anem
-   # Vitesse vent en km/h
-   speed_kmph = speed_mps * 3.6
+#led
+n = 48
+p = 5
+maxLight = 30
+r = 200
+g = 200
+b = 200
 
-   print( "value: ", value )
-   print( "m/s:", speed_mps )
-   print( "km/h:", speed_kmph )
-   print( "--------------------" )
-   if speed_kmph > 5:
-      uart.write("esp1On")
-   sleep( 0.5 )
+while True:
+    if isActivated == 0:
+        value = ana.read_uv() # 0..4095
+        value = value/1000
+        #value2 = ana2.read()
+        v_esp = 3.3 * value / 4096
+        v_anem = v_esp / R_BRIDGE_RATIO
+        # Vitesse vent m/h
+        speed_mps = 6 * v_anem
+        # Vitesse vent en km/h
+        speed_kmph = speed_mps * 3.6
+
+        print( "value 27: ", value )
+        print("pin : ", pinAnemo.value())
+        print( "m/s:", speed_mps )
+        print( "km/h:", speed_kmph )
+        print( "--------------------" )
+        if speed_kmph > 5:
+          uart.write("esp1On")
+        sleep( 0.2 )
+    else:
+        np = neopixel.NeoPixel(Pin(p),n)
+        for i in range(maxLight):
+          np[i] = (r, g, b)
+          np.write()
+    
 
 uart.close()
+
